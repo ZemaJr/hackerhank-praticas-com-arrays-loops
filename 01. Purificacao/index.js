3805e34b9a34b2a721292d1db7aebe4f5ff37d6e
ZemaJr <zema.eng.el@gmail.com>
2023-07-04 14:57:52 -0300

    Resolucao 01

---
+++ 01. Purificacao/index.js
@@ -0,0 +1,20 @@
+function solucao(stringCorrompida) {
+  const caracteresCorrompidos = "!@#$%&*().";
+  let dadoPurificado = "";
+
+  for (const caracter of stringCorrompida) {
+    let existe = false;
+
+    for (const caracterCorrompido of caracteresCorrompidos) {
+      if (caracter == caracterCorrompido) {
+        existe = true;
+        break;
+      }
+    }
+
+    if (!existe) {
+      dadoPurificado += caracter;
+    }
+  }
+  console.log(dadoPurificado);
+}
